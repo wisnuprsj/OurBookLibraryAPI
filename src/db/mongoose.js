2d9dd6cd2caca6bf60cba2a4fb6ad0c19e5460ce
Wisnu Prasojo Widianto <wisnuprsj@gmail.com>
2021-01-17 22:49:58 +0700

    OurLibraryAPI V1 > V2 Reason: move mongodb uri to env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const uri =
-  "mongodb+srv://admin:admin@cluster0.r6zfg.mongodb.net/BookLibraryDB?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 
-const uri2 = "mongodb://127.0.0.1:27017/BookLibraryDB";
+// const uri2 = "mongodb://127.0.0.1:27017/BookLibraryDB";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
