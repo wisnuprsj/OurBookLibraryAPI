7d275ea49f8acdb7ffff4cea70f2429c1b810279
Wisnu Prasojo Widianto <wisnuprsj@gmail.com>
2021-01-19 03:53:36 +0700

    change env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// const uri = process.env.MONGODB_URI;
-const uri = process.env.MONGODB_URI_PROD;
+const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI_PROD;
 
 // const uri2 = "mongodb://127.0.0.1:27017/BookLibraryDB";
 
